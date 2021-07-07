0403116a9952b7c8e136a24e3ddf11661d94e904
Yakushev Ilya <coder4@web-axioma.ru>
2021-07-07 11:45:32 +0700

    add Title Form(atributes)

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Список Дел</title>
+    <link rel="shorcut icon" href="/img/icon/001-to-do-list.png" type="image/png">
+    <link rel="stylesheet" href="/css/style.css" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" type="text/css">
+
+</head>
+<body>
+<div class="container">
+    <h1>Список дел</h1>
+    <form action="add.php" method="post">
+        <input type="text" name="task" id="task" placeholder="Нужно сделать...">
+    </form>
+</div>
+
+</body>
+</html>
